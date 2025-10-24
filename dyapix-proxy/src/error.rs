44d6a31aaba8d984c85d25832ea94d9edec0ddd3
cj2a7t <cj2act@gmail.com>
2025-10-25 07:57:31 +0800

    v0.4.0: support feature: match route and upstream

---
+++ dyapix-proxy/src/error.rs
@@ -0,0 +1,13 @@
+pub const ERROR_ROUTE_NOT_FOUND: &str = "Route Not Found";
+pub const ERROR_UPSTREAM_NOT_FOUND: &str = "Upstream Not Found";
+pub const ERROR_CACHE_NOT_INITIALIZED: &str = "Cache Not Initialized";
+
+pub trait AnyhowResultExt<T> {
+    fn to_pingora_result(self, message: &'static str) -> pingora::Result<T>;
+}
+
+impl<T> AnyhowResultExt<T> for anyhow::Result<T> {
+    fn to_pingora_result(self, message: &'static str) -> pingora::Result<T> {
+        self.map_err(|_| pingora::Error::new(pingora::ErrorType::Custom(message)))
+    }
+}
