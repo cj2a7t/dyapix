b246e631b7dfe7eed346ca780d680c161564ef44
cj2a7t <cj2act@gmail.com>
2025-10-26 20:38:02 +0800

    fea: support log & startup

---
+++ dyapix-common/src/log/log.rs
@@ -0,0 +1,36 @@
+use anyhow::Result;
+use tracing_appender::rolling::RollingFileAppender;
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+
+use crate::config::get_app_config;
+
+pub fn init_logging() -> Result<impl Drop> {
+    let config = get_app_config()?;
+
+    let file_appender: RollingFileAppender =
+        tracing_appender::rolling::daily(config.log.log_dir.clone(), config.log.log_name.clone());
+    let (file_writer, guard) = tracing_appender::non_blocking(file_appender);
+
+    let console_layer = fmt::layer()
+        .with_writer(std::io::stdout)
+        .with_target(true)
+        .with_level(true)
+        .pretty();
+
+    let json_file_layer = fmt::layer()
+        .json()
+        .with_writer(file_writer)
+        .with_current_span(true)
+        .with_span_events(fmt::format::FmtSpan::CLOSE);
+
+    let filter_layer = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new(config.log.log_level.clone()));
+
+    tracing_subscriber::registry()
+        .with(filter_layer)
+        .with(console_layer)
+        .with(json_file_layer)
+        .init();
+
+    Ok(guard)
+}
--- dyapix/src/main.rs
+++ dyapix/src/main.rs
@@ -1,5 +1,41 @@
-fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
+use anyhow::Result;
+use dyapix_common::{config::get_app_config, log::log::init_logging};
+use dyapix_proxy::{background_service::DyapixBackgroundService, proxy::DyapixProxy};
+use pingora::{
+    proxy::http_proxy_service,
+    server::{Server, configuration::Opt},
+    services::background::background_service,
+};
+use tracing::info;
 
-    Ok(())
+fn main() -> Result<()> {
+    // Initialize configuration
+    let config = get_app_config()?;
+
+    // Initialize logging
+    let _guard = init_logging()?;
+
+    // Create server
+    let opt = Some(Opt::default());
+    let mut dyapix = Server::new(opt)?;
+    dyapix.bootstrap();
+
+    // Start background service
+    let background_service =
+        background_service("dyapix_background", DyapixBackgroundService::new());
+    info!("Background service created");
+
+    // Add services to server
+    dyapix.add_service(background_service);
+
+    // Start proxy service
+    let mut proxy_service = http_proxy_service(&dyapix.configuration, DyapixProxy);
+    let proxy_addr = format!("{}:{}", config.server.proxy_host, config.server.proxy_port);
+    proxy_service.add_tcp(&proxy_addr);
+    dyapix.add_service(proxy_service);
+    info!("Proxy service created and listening on {}", proxy_addr);
+
+    // Start the server
+    info!("Starting Dyapix server...");
+    dyapix.run_forever();
 }
